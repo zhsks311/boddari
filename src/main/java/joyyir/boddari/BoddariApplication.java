641940a4f436190c8c3625b5c0679b5555c6557a
Jang Junyeong <joyyir@naver.com>
2021-03-01 00:25:09 +0900

    initial commit

---
+++ src/main/java/joyyir/boddari/BoddariApplication.java
@@ -0,0 +1,11 @@
+package joyyir.boddari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoddariApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoddariApplication.class, args);
+    }
+}
---
+++ src/test/java/joyyir/boddari/BoddariApplicationTests.java
@@ -0,0 +1,11 @@
+package joyyir.boddari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoddariApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
