58ba7b304b7d31a0e81c9d29e8f8e4027aaddfae
Jang Junyeong <joyyir@naver.com>
2021-09-30 00:39:07 +0900

    /start 명령어 필요함 (봇 초기 실행 시)

--- src/main/java/joyyir/boddari/interfaces/handler/BoddariBotHandler.java
+++ src/main/java/joyyir/boddari/interfaces/handler/BoddariBotHandler.java
@@ -40,30 +40,15 @@ public class BoddariBotHandler extends TelegramLongPollingBot {
             try {
                 String[] commands = update.getMessage().getText().split(" ");
                 switch (commands[0]) {
+                    case "/start":
+                    case "/help":
+                        help(chatId);
+                        break;
                     case "/user":
                         this.userController.runCommand(chatId, commands, this);
                         break;
                     case "/trade":
                         this.tradeController.runCommand(chatId, commands, this);
-                        break;
-                    case "/help":
-                        String message =
-                            "처음 오신 분은 /user register 명령으로 유저 등록하시고 /trade start 명령으로 트레이딩을 시작하세요.\n" +
-                            "\n" +
-                            "/user register : 유저 등록\n" +
-                            "/user unregister : 유저 정보 제거\n" +
-                            "/user info : 유저 정보 조회\n" +
-                            "\n" +
-                            "/trade start : 새로운 트레이딩 시작\n" +
-                            "/trade stop : 현재 트레이딩 종료\n" +
-                            "/trade pause : 현재 트레이딩 일시 중지\n" +
-                            "/trade resume : 일시 중지된 트레이딩 재개\n" +
-                            "/trade status : 현재 진행 중인 트레이딩 상태 확인\n" +
-                            "/trade history : 최근 n일 동안 진행된 트레이드 히스토리 확인 (예시) /trade history 5\n" +
-                            "\n" +
-                            "문의 사항은 joyyir@naver.com으로 보내주세요.\n" +
-                            "";
-                        sendMessage(chatId, message);
                         break;
                     default:
                         throw new BadRequestException("지원하지 않는 명령어입니다.");
@@ -94,5 +79,25 @@ public class BoddariBotHandler extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         return "보따리봇";
+    }
+
+    private void help(Long chatId) {
+        String message =
+            "처음 오신 분은 /user register 명령으로 유저 등록하시고 /trade start 명령으로 트레이딩을 시작하세요.\n" +
+                "\n" +
+                "/user register : 유저 등록\n" +
+                "/user unregister : 유저 정보 제거\n" +
+                "/user info : 유저 정보 조회\n" +
+                "\n" +
+                "/trade start : 새로운 트레이딩 시작\n" +
+                "/trade stop : 현재 트레이딩 종료\n" +
+                "/trade pause : 현재 트레이딩 일시 중지\n" +
+                "/trade resume : 일시 중지된 트레이딩 재개\n" +
+                "/trade status : 현재 진행 중인 트레이딩 상태 확인\n" +
+                "/trade history : 최근 n일 동안 진행된 트레이드 히스토리 확인 (예시) /trade history 5\n" +
+                "\n" +
+                "문의 사항은 joyyir@naver.com으로 보내주세요.\n" +
+                "";
+        sendMessage(chatId, message);
     }
 }
